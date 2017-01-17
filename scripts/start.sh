1efa22585eb4a2700cd60cd9994d865d06cf3063
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2017-01-17 19:48:10 +0100

    Add -p flag to start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,16 @@
 script_dir="${0%/*}"
 cd $script_dir/..
 
+db_port=5432
+
+while getopts 'p:' flag; do
+  case "${flag}" in
+    p) db_port="${OPTARG}" ;;
+  esac
+done
+
+sed "s/{{DB_PORT}}/$db_port/g" docker-compose.tmpl > docker-compose.yml
+
 echo "Stopping existing containers..."
 docker-compose down
 
